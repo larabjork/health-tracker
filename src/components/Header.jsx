b4d4e0a2a83394dd2e1d0a18e42ca8e1fcee5df0
Lara Bjork <lara.m.bjork@gmail.com>
2020-03-05 10:54:05 -0800

    add fonts and colors to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button }from 'react-bootstrap';
+import { Navbar, Nav }from 'react-bootstrap';
+import './../Header.css';
 
 function Header(){
   const HeaderGrid = {
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
-    borderTop: '8px solid #141414',
-    borderBottom: '8px solid #141414',
-    backgroundColor: '#596FFF'
+    borderTop: '8px solid #210124',
+    borderBottom: '8px solid #210124',
+    backgroundColor: '#E5FCF5'
   }
   const btnStyle = {
-    backgroundColor: '#141414',
-    borderColor: '#141414',
+    backgroundColor: '#210124',
+    borderColor: '#210124',
     color: 'white',
     fontWeight: 'bold'
   }
@@ -21,20 +22,13 @@ function Header(){
   return (
     <>
     <Navbar style={HeaderGrid}>
-      <Navbar.Brand href="#home" className="tavernName">Tip Top Taps</Navbar.Brand>
+      <Navbar.Brand href="#home" className="brandName">MS Journal</Navbar.Brand>
       <div id="header">
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#keglist">Keg List</Nav.Link>
-            <NavDropdown title="Kegmeisters Only" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#admin">Admin Area</NavDropdown.Item>
-              <NavDropdown.Item href="#newkegform">Add Keg</NavDropdown.Item>
-              <NavDropdown.Item href="#editkegform">Edit Keg List</NavDropdown.Item>
-            </NavDropdown>
-            <Form inline>
-              <FormControl type="text" placeholder="Show me a keg..." className="mr-sm-2" />
-              <Button style={btnStyle}>Search</Button>
-            </Form>
+            <Nav.Link href="#medhistory">What I Tell Doctors</Nav.Link>
+            <Nav.Link href="#mslife">Journaling My Life with MS</Nav.Link>
+            <Nav.Link href="#mslife">My Personal MS Milestones</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </div>
