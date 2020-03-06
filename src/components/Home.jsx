b6be926c5022a1248fe8ba86651fea4606ade5a5
Lara Bjork <lara.m.bjork@gmail.com>
2020-03-06 10:03:19 -0800

    add placeholders for design ideas for home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,10 @@ function Home(){
   return (
     <div>
       <Container>
-        <h1>Multiple sclerosis is different for everyone who has it.</h1>
+        <h1>You are more than your diagnosis. </h1>
+        <h2>And you have more than one story to tell about your life with MS.</h2>
+        <h3>button to signin  button to learn more</h3>
+        <h3> icon1 for doctor + icon2 for selfcare + icon3 for journal</h3>
       </Container>
     </div>
   )
